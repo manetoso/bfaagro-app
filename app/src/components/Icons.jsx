bbf8da7450e139141afb3188e09b186ecba61d35
“manetoso” <manetoso98@hotmail.com>
2023-03-15 16:48:34 -0600

    feat: agregando iconos

--- app/src/components/Icons.jsx
+++ app/src/components/Icons.jsx
@@ -54,3 +54,56 @@ export function ChevronBack() {
     </svg>
   )
 }
+
+export function ChevronRight() {
+  return (
+    <svg
+      height='24'
+      width='24'
+      fill='currentColor'
+      viewBox='0 0 20 20'
+      xmlns='http://www.w3.org/2000/svg'
+      aria-hidden='true'
+    >
+      <path
+        clipRule='evenodd'
+        fillRule='evenodd'
+        d='M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z'
+      />
+    </svg>
+  )
+}
+
+export function DotsHorizontal() {
+  return (
+    <svg
+      height='24'
+      width='24'
+      fill='currentColor'
+      viewBox='0 0 20 20'
+      xmlns='http://www.w3.org/2000/svg'
+      aria-hidden='true'
+    >
+      <path d='M6 10a2 2 0 11-4 0 2 2 0 014 0zM12 10a2 2 0 11-4 0 2 2 0 014 0zM16 12a2 2 0 100-4 2 2 0 000 4z' />
+    </svg>
+  )
+}
+
+export function Filter() {
+  return (
+    <svg
+      height='24'
+      width='24'
+      fill='currentColor'
+      viewBox='0 0 20 20'
+      xmlns='http://www.w3.org/2000/svg'
+      aria-hidden='true'
+    >
+      <path
+        clipRule='evenodd'
+        fillRule='evenodd'
+        d='M3 3a1 1 0 011-1h12a1 1 0 011 1v3a1 1 0 01-.293.707L12 11.414V15a1 1 0 01-.293.707l-2 2A1 1 0 018 17v-5.586L3.293 6.707A1 1 0 013 6V3z'
+      />
+    </svg>
+  )
+}
