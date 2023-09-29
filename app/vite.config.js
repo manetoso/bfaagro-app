bc9892fa3a2f37b30dfd593564c71128dc4abbe1
manetoso <manetoso98@hotmail.com>
2023-09-28 19:15:53 -0600

    fix: PWA 2
    Intento No.2

--- app/vite.config.js
+++ app/vite.config.js
@@ -14,5 +14,51 @@ export default defineConfig({
       }
     ]
   },
-  plugins: [react(), VitePWA({ registerType: 'autoUpdate' })]
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'prompt',
+      includeAssets: [
+        'bfaagro.ico',
+        'favicon.ico',
+        'apple-touch-icon.png',
+        'masked-icon.svg'
+      ],
+      manifest: {
+        name: 'BFA Agro app',
+        short_name: 'BFA Agro',
+        description: 'Aplicación interna de BFA Agro.',
+        icons: [
+          {
+            src: '/android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: '/apple-touch-icon.png',
+            sizes: '180x180',
+            type: 'image/png',
+            purpose: 'apple touch icon'
+          },
+          {
+            src: '/maskable_icon.png',
+            sizes: '225x225',
+            type: 'image/png',
+            purpose: 'any maskable'
+          }
+        ],
+        theme_color: '#0C923F',
+        background_color: '#204983',
+        display: 'standalone',
+        scope: '/',
+        start_url: '/autenticacion',
+        orientation: 'portrait'
+      }
+    })
+  ]
 })
