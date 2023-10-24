95e3b51cac71e474d28b5e18fb5945d6a5ed5b4b
manetoso <manetoso98@hotmail.com>
2023-10-24 16:46:29 -0600

    feat: Notification
    Añadiendo el boton de notificaciones

--- app/src/components/navigation/Navigation.jsx
+++ app/src/components/navigation/Navigation.jsx
@@ -2,35 +2,47 @@ import { useId, useRef } from 'react'
 import { useNavigate } from 'react-router-dom'
 
 import { Aside, IconButton } from './'
+import { NotificationDropDown } from './NotificationDropDown'
+
 import { ChevronBack, Menu } from '🚀'
 
 import './navigation.css'
+import { useAuthStore } from '@/stores'
+import { ROLES } from '@/utils/consts'
 
 export function Navigation() {
   const menuCheckboxId = useId()
   const inputRef = useRef()
   const navigate = useNavigate()
+  const { roles } = useAuthStore()
   return (
     <>
       <nav className='fixed top-0 z-50 w-screen border-b-2 border-black bg-white'>
-        <div className='mx-auto flex max-w-[1000px] gap-2 p-2'>
-          <IconButton htmlFor={menuCheckboxId}>
-            <Menu />
-          </IconButton>
-          {!window.location.pathname.includes('app/inicio') && (
-            <>
-              <IconButton onClick={() => navigate('/app/inicio')}>
-                <ChevronBack /> Inicio
-              </IconButton>
-            </>
-          )}
-          {!window.location.pathname.includes('app/inicio') && (
-            <>
-              <IconButton onClick={() => window.history.back()}>
-                <ChevronBack />
-                Atras
-              </IconButton>
-            </>
+        <div className='mx-auto flex max-w-[1000px] justify-between p-2'>
+          <div className='flex gap-2'>
+            <IconButton htmlFor={menuCheckboxId}>
+              <Menu />
+            </IconButton>
+            {!window.location.pathname.includes('app/inicio') && (
+              <>
+                <IconButton onClick={() => navigate('/app/inicio')}>
+                  <ChevronBack /> Inicio
+                </IconButton>
+              </>
+            )}
+            {!window.location.pathname.includes('app/inicio') && (
+              <>
+                <IconButton onClick={() => window.history.back()}>
+                  <ChevronBack />
+                  Atras
+                </IconButton>
+              </>
+            )}
+          </div>
+          {(roles.includes(ROLES.ADMIN) || roles.includes(ROLES.SALES)) && (
+            <span>
+              <NotificationDropDown />
+            </span>
           )}
         </div>
       </nav>
